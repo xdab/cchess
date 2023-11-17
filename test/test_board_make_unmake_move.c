06212f58dd7a9f544fd05d5df752ff1d2e0c8831
PW <przemyslawxdab@gmail.com>
2023-11-17 21:30:11 +0100

    Test side to move flips after making a move

--- test/test_board_make_unmake_move.c
+++ test/test_board_make_unmake_move.c
@@ -29,12 +29,16 @@ void check_regular_move(board_t *board)
 		test_error("After move e4, e4 did not contain a white pawn");
 	if (board_get(board, SQUARE_E2) != PIECE_NONE)
 		test_error("After move e4, e2 was not empty");
+	if (board->side_to_move != SIDE_BLACK)
+		test_error("After move 1. e4, side to move was not black");
 
 	board_unmake_move(board);
 	if (board_get(board, SQUARE_E2) != (PIECE_PAWN | SIDE_WHITE))
 		test_error("After unmaking e4, white pawn from e4 did not return to e2");
 	if (board_get(board, SQUARE_E4) != PIECE_NONE)
 		test_error("After unmaking e4, e4 was not empty");
+	if (board->side_to_move != SIDE_WHITE)
+		test_error("After unmaking 1. e4, side to move was not white");
 }
 
 void check_capture(board_t *board)
@@ -48,12 +52,16 @@ void check_capture(board_t *board)
 		test_error("After (1. e4 d5 2. exd5), d5 did not contain a white pawn");
 	if (board_get(board, SQUARE_E4) != PIECE_NONE)
 		test_error("After (1. e4 d5 2. exd5), e4 was not empty");
+	if (board->side_to_move != SIDE_BLACK)
+		test_error("After (1. e4 d5 2. exd5), side to move was not black");
 
 	board_unmake_move(board);
 	if (board_get(board, SQUARE_E4) != (PIECE_PAWN | SIDE_WHITE))
 		test_error("After unmaking (2. exd5), white pawn from d5 did not return to e4");
 	if (board_get(board, SQUARE_D5) != (PIECE_PAWN | SIDE_BLACK))
 		test_error("After unmaking (2. exd5), a black pawn was not returned to d5");
+	if (board->side_to_move != SIDE_WHITE)
+		test_error("After unmaking (2. exd5), side to move was not white");
 }
 
 void check_promotion(board_t *board)
@@ -75,4 +83,15 @@ void check_promotion(board_t *board)
 		test_error("After (1. c4 c5 2. d4 cxd4 3. c5 d6 4. c6 Bd7 5. c7 Be6 6. c8=Q), c8 did not contain a white queen");
 	if (board_get(board, SQUARE_C7) != PIECE_NONE)
 		test_error("After (1. c4 c5 2. d4 cxd4 3. c5 d6 4. c6 Bd7 5. c7 Be6 6. c8=Q), c7 was not empty");
+	if (board->side_to_move != SIDE_BLACK)
+		test_error("After (1. c4 c5 2. d4 cxd4 3. c5 d6 4. c6 Bd7 5. c7 Be6 6. c8=Q), side to move was not black");
+	
+	board_unmake_move(board);
+	board_print(board, stderr);
+	if (board_get(board, SQUARE_C7) != (PIECE_PAWN | SIDE_WHITE))
+		test_error("After unmaking (6. c8=Q), white queen from c8 did not return to c7 as a pawn");
+	if (board_get(board, SQUARE_C8) != PIECE_NONE)
+		test_error("After unmaking (6. c8=Q), c8 was not empty");
+	if (board->side_to_move != SIDE_WHITE)
+		test_error("After unmaking (6. c8=Q), side to move was not white");
 }
